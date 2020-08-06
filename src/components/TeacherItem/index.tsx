96d1d1f1da222b5685a7d9a1c1af5ada2f003b65
CaueFurui <cauefurui@gmail.com>
2020-08-06 17:28:25 -0300

    [TEACHERLIST]Integrated with API

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,34 +1,51 @@
 import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string,
+  bio: string,
+  cost: number,
+  id: number,
+  name: string,
+  subject: string,
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection () {
+    api.post('connections', { user_id: teacher.id })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/48840933?s=460&u=398aa25c197720153fd9bc152fa16f3f416cb853&v=4" alt="Avatar"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>   
-          <strong>Caue Furui Martins</strong>
-          <span>Física</span>
+          <strong>{ teacher.name }</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-      Entusiasta das melhores tecnologias de química avançada.
-      <br /><br />
-      Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através 
-      de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 40,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type='button'>
+        <a target='_blank' href={`https://wa.me/+55${teacher.whatsapp}`} type='button' onClick={createNewConnection}>
           <img src={whatsappIcon} alt="WhatsApp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
 
     </article>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,36 +1,69 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 import './styles.css'
 import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
 import * as DATA from './TeacherList.json'
 import Input from '../../components/Input'
 import Select from '../../components/Select'
+import api from '../../services/api'
 
 function TeacherList() {
+
+  const [teachers, setTeachers] = useState([])
+  const [subject, setSubject] = useState('')
+  const [week_day, setWeekDay] = useState('')
+  const [time, setTime] = useState('')
+
+  async function handleSearchTeachers(e: FormEvent) {
+    e.preventDefault()
+
+    const res = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    })
+
+    setTeachers(res.data)
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title={DATA.title}>
-        <form id="search-teachers">
-        <Select 
+        <form id="search-teachers" onSubmit={handleSearchTeachers}>
+          <Select 
             name='subject' 
             label='Matéria' 
             options={DATA.subjectData}
-            />
+            value={subject}
+            onChange={(e) => { setSubject(e.target.value) }}
+          />
           <Select 
             name='week_day' 
             label='Dia da semana' 
             options={DATA.weekDays}
-            />
-          <Input name='time' type='time' label='Hora' />
+            value={week_day}
+            onChange={(e) => { setWeekDay(e.target.value) }}
+          />
+          <Input
+            name='time'
+            type='time'
+            label='Hora'
+            value={time}
+            onChange={
+              (e) => { setTime(e.target.value) 
+            }}
+          />
+
+          <button type='submit'>Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        { teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />
+        }) }
       </main>
     </div>
   )
