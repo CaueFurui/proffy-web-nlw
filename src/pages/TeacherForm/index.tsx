7ee1a560704475e47b297ade2df8092999dd7c8f
CaueFurui <cauefurui@gmail.com>
2020-08-06 15:33:55 -0300

    [GIVECLASSES]Adding function to add mora than one schedule to give classes

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from '../../components/PageHeader'
 import * as DATA from './TeacherForm.json'
 import './styles.css'
@@ -8,6 +8,24 @@ import Textarea from '../../components/Textarea'
 import Select from '../../components/Select'
 
 function TeacherForm() {
+
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: '0', from: '', to: '' }
+  ])
+
+  function addNewScheduleItem() {
+    setScheduleItems([
+      ...scheduleItems,
+      { week_day: '0', from: '', to: '' }
+    ])
+
+    scheduleItems.push({
+      week_day: '',
+      from: '',
+      to: ''
+    })
+  }
+
   return(
     <div id="page-teacher-form" className="container">
       <PageHeader 
@@ -31,6 +49,30 @@ function TeacherForm() {
             options={DATA.subjectData}
             />
           <Input name='cost' label='Custo da sua aula por hora' />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+              <button type='button' onClick={addNewScheduleItem}>
+                + Novo Horário
+              </button>
+          </legend>
+
+          {scheduleItems.map(scheduleItem => {
+            return (
+              <div key={scheduleItem.week_day} className="schedule-item">
+                <Select 
+                  name='week_day' 
+                  label='Dia da semana' 
+                  options={DATA.weekDays}
+                  />
+                <Input type="time" name='from' label='Das'/>
+                <Input type="time" name='to' label='Até'/>
+              </div>
+            )
+          })}
+
         </fieldset>
 
         <footer>
