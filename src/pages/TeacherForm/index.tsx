88b8dff0ba8ffe0302ad43027c30fc7762b0643c
CaueFurui <cauefurui@gmail.com>
2020-08-05 21:31:57 -0300

    [TEACHERFORM]Adding style to the page

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -3,6 +3,7 @@ import PageHeader from '../../components/PageHeader'
 import * as DATA from './TeacherForm.json'
 import './styles.css'
 import Input from '../../components/Input'
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 function TeacherForm() {
   return(
@@ -14,10 +15,27 @@ function TeacherForm() {
       <main>
         <fieldset>
           <legend>Seus dados</legend>
-          <Input name='subject' label='Matéria' />
+          <Input name='name' label='Nome Completo' />
           <Input name='avatar' label='Avatar' />
           <Input name='whatsapp' label='WhatsApp' />
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input name='subject' label='Matéria' />
+          <Input name='cost' label='Custo da sua aula por hora' />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso"/>
+            Importante! <br/>
+            Preencha todos os dados
+          </p>
+          <button type='button'>
+            Salvar cadastro
+          </button>
+        </footer>
       </main>
     </div>
   )
